69319cdc5971afa2f4b49326a7120620294d869b
Daniel Jurgens <danielj@mellanox.com>
2017-05-22 16:08:30 +0300

    semanage: Update semanage to allow runtime labeling of ibendports
    Update libsepol and libsemanage to work with ibendport records. Add local
    storage for new and modified ibendport records in ibendports.local.
    Update semanage to parse the ibendport command options to add, modify,
    and delete them.
    
    Signed-off-by: Daniel Jurgens <danielj@mellanox.com>
    
    (cherry picked from SELinuxProject commit 9a3d2c7a9b78f7ca04977a4c5a6bda4c8875c89c)

--- libsepol/include/sepol/sepol.h
+++ libsepol/include/sepol/sepol.h
@@ -11,6 +11,7 @@ __BEGIN_DECLS
 #include <sepol/context_record.h>
 #include <sepol/iface_record.h>
 #include <sepol/ibpkey_record.h>
+#include <sepol/ibendport_record.h>
 #include <sepol/port_record.h>
 #include <sepol/boolean_record.h>
 #include <sepol/node_record.h>
@@ -18,6 +19,7 @@ __BEGIN_DECLS
 #include <sepol/booleans.h>
 #include <sepol/interfaces.h>
 #include <sepol/ibpkeys.h>
+#include <sepol/ibendports.h>
 #include <sepol/ports.h>
 #include <sepol/nodes.h>
 #include <sepol/users.h>
