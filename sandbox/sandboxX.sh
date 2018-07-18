2e9e64c6244c675dc1fd218fb597947d59307ff8
Petr Lautrbach <plautrba@redhat.com>
2018-07-18 09:09:35 +0200

    sandbox: Use matchbox-window-manager instead of openbox

--- sandbox/sandboxX.sh
+++ sandbox/sandboxX.sh
@@ -7,20 +7,6 @@ export TITLE="Sandbox $context -- `grep ^#TITLE: ~/.sandboxrc | /usr/bin/cut -b8
 [ -z $3 ] && export DPI="96" || export DPI="$3"
 trap "exit 0" HUP
 
-mkdir -p ~/.config/openbox
-cat > ~/.config/openbox/rc.xml << EOF
-<openbox_config xmlns="http://openbox.org/3.4/rc"
-		xmlns:xi="http://www.w3.org/2001/XInclude">
-<applications>
-  <application class="*">
-    <decor>no</decor>
-    <desktop>all</desktop>
-    <maximized>yes</maximized>
-  </application>
-</applications>
-</openbox_config>
-EOF
-
 if [ "$WAYLAND_NATIVE" == "no" ]; then
     if [ -z "$WAYLAND_DISPLAY" ]; then
         DISPLAY_COMMAND='/usr/bin/Xephyr -resizeable -title "$TITLE" -terminate -screen $SCREENSIZE -dpi $DPI -nolisten tcp -displayfd 5 5>&1 2>/dev/null'
