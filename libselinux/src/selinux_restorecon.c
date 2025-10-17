25721e379bc2544ad8b0f2cadcd9b3dce87a8422
Vit Mojzis <vmojzis@redhat.com>
2025-10-17 17:08:34 +0200

    libselinux: Ignore files removed during relabeling
    In case ignore_noent is specified, ignore files removed during
    relabeling (race condition between folder read, file read and label
    set).
    
    Signed-off-by: Vit Mojzis <vmojzis@redhat.com>
    Acked-by: Stephen Smalley <stephen.smalley.work@gmail.com>

--- libselinux/src/selinux_restorecon.c
+++ libselinux/src/selinux_restorecon.c
@@ -702,6 +702,9 @@ static int restorecon_sb(const char *pathname, const struct stat *sb,
 			    pathname, newcon);
 
 	if (lgetfilecon_raw(pathname, &curcon) < 0) {
+		/* Ignore files removed during relabeling if ignore_noent is set */
+		if (flags->ignore_noent && errno == ENOENT)
+			goto out;
 		if (errno != ENODATA)
 			goto err;
 
@@ -736,8 +739,14 @@ static int restorecon_sb(const char *pathname, const struct stat *sb,
 		}
 
 		if (!flags->nochange) {
-			if (lsetfilecon(pathname, newcon) < 0)
-				goto err;
+			if (lsetfilecon(pathname, newcon) < 0) {
+				/* Ignore files removed during relabeling if ignore_noent is set */
+				if (flags->ignore_noent && errno == ENOENT)
+					goto out;
+				else
+					goto err;
+			}
+
 			updated = true;
 		}
 
@@ -903,9 +912,10 @@ loop_body:
 		case FTS_NS:
 			error = errno;
 			errno = ftsent->fts_errno;
-			selinux_log(SELINUX_ERROR,
-				    "Could not stat %s: %m.\n",
-				    ftsent->fts_path);
+			if (!state->flags.ignore_noent || errno != ENOENT)
+				selinux_log(SELINUX_ERROR,
+					    "Could not stat %s: %m.\n",
+					    ftsent->fts_path);
 			errno = error;
 			fts_set(fts, ftsent, FTS_SKIP);
 			continue;
